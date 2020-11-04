4de43180ee7d248a0465b59b8ee9c300ffebdefc
rynlu <47727459+rynlu@users.noreply.github.com>
2020-11-03 21:36:14 -0500

    Added conditionals for all periods

--- script.js
+++ script.js
@@ -1,58 +1,134 @@
-function gettime(x) {
-    var d = new Date();
+function gettime() {
 
+    console.log('mainloop working')
+
+    var d = new Date();
     hour = d.getHours()
     minutes = d.getMinutes()
     seconds = d.getSeconds()
     day = d.getDay()
 
-    switch(day) {
-        case 0:
-            document.getElementById("time").innerHTML = "It's the weekend... go back to sleep.";
-            break;
-        case 6:
-            document.getElementById("time").innerHTML = "It's the weekend... go back to sleep.";
-            break;
-        default:
-            switch(0 < hour < 8 || 3 < hour < 23) {
-                case true:
-                    document.getElementById("time").innerHTML = "Classes over.";
-                default:
-                    if (hour == 9 && minutes >= 10) {
-                        document.getElementById("time").innerHTML = (60 - minutes + 5) + "minutes left in period " + (x + 1);
-                    } else if (hour == 10 && minutes <= 5) {
-                        document.getElementById("time").innerHTML = (5 - minutes) + "minutes left in period " + (x + 1);
-                    }
-            }
+    if (day == 0 || day == 6) {
+        document.getElementById("time").innerHTML = "It's the weekend, go back to sleep.";
+
+    } else if (0 < hour && hour < 8) {
+        document.getElementById("time").innerHTML = ((9 - hour) * 60 + (10 - minutes)) + " minutes until period " + (window.whichDay + 1);
+   
+    } else if (hour == 9 && minutes == 9 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second until period " + (window.whichDay + 1);
+    } else if (hour == 9 && minutes == 9) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds until period " + (window.whichDay + 1);
+    } else if (hour == 9 && minutes < 10) {
+        document.getElementById("time").innerHTML = (10 - minutes) + " minutes until period " + (window.whichDay + 1);
+
+    } else if (hour == 9) {
+        document.getElementById("time").innerHTML = (60 - minutes + 5) + " minutes left in period " + (window.whichDay + 1);
+    } else if (hour == 10 && minutes < 4) {
+        document.getElementById("time").innerHTML = (5 - minutes) + " minutes left in period " + (window.whichDay + 1);
+    } else if (hour == 10 && minutes == 4 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second left in period " + (window.whichDay + 1);
+    } else if (hour == 10 && minutes == 4) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds left in period " + (window.whichDay + 1);
+
+    } else if (hour == 10 && minutes == 14 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second until period " + (window.whichDay + 2);
+    } else if (hour == 10 && (minutes == 14)) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds until period " + (window.whichDay + 2);
+    } else if (hour == 10 && (minutes > 5 && minutes < 14)) {
+        document.getElementById("time").innerHTML = (15 - minutes) + " minutes until period " + (window.whichDay + 2);
+
+
+    } else if (hour == 10 && minutes >= 15) {
+        document.getElementById("time").innerHTML = (70 - minutes) + " minutes left in period " + (window.whichDay + 2);
+    } else if (hour == 11 && minutes == 9 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second left in period " + (window.whichDay + 2);
+    } else if (hour == 11 && minutes == 9) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds left in period " + (window.whichDay + 2);
+    } else if (hour == 11 && minutes < 9) {
+        document.getElementById("time").innerHTML = (10 - minutes) + " minutes left in period " + (window.whichDay + 2);
+
+    } else if (hour == 11 && minutes == 19 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second until period " + (window.whichDay + 3);
+    } else if (hour == 11 && minutes == 19) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds until period " + (window.whichDay + 3);
+    } else if (hour == 11 && minutes < 20) {
+        document.getElementById("time").innerHTML = (20 - minutes) + " minutes until period " + (window.whichDay + 3);
+
+    } else if (hour == 11) {
+        document.getElementById("time").innerHTML = (75 - minutes) + " minutes left in period " + (window.whichDay + 3);
+    } else if (hour = 12 && minutes == 14 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second left in period " + (window.whichDay + 3);
+    } else if (hour = 12 && minutes == 14) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds left in period " + (window.whichDay + 3);
+    } else if (hour = 12 && minutes < 15) {
+        document.getElementById("time").innerHTML = (15 - minutes) + " minutes left in period " + (window.whichDay + 3);
+    
+    } else if (hour = 12 && minutes == 24 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second until period " + (window.whichDay + 4);
+    } else if (hour = 12 && minutes == 24) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds until period " + (window.whichDay + 4);
+    } else if (hour == 12 && minutes < 25) {
+        document.getElementById("time").innerHTML = (25 - minutes) + " minutes until period " + (window.whichDay + 4);
+
+    } else if (hour == 12) {
+        document.getElementById("time").innerHTML = (80 - minutes) + " minutes left in period " + (window.whichDay + 4);
+    } else if (hour == 13 && minutes == 19 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second left in period " + (window.whichDay + 4);
+    } else if (hour == 13 && minutes == 19) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds left in period " + (window.whichDay + 4);
+    } else if (hour == 13 && minutes < 20) {
+        document.getElementById("time").innerHTML = (20 - minutes) + " minutes left in period " + (window.whichDay + 4);
+
+    } else if (hour == 13 && minutes == 29 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second until period " + (window.whichDay + 5);
+    } else if (hour == 13 && minutes == 29) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds until period " + (window.whichDay + 5);
+    } else if (hour == 13 && minutes < 30) {
+        document.getElementById("time").innerHTML = (30 - minutes) + " minutes until period " + (window.whichDay + 5);
+
+    } else if (hour == 13) {
+        document.getElementById("time").innerHTML = (85 - minutes) + " minutes left in period " + (window.whichDay + 5);
+    } else if (hour == 14 && minutes == 24 && seconds == 59) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " second left in period " + (window.whichDay + 5);
+    } else if (hour == 14 && minutes == 24) {
+        document.getElementById("time").innerHTML = (60 - seconds) + " seconds left in period " + (window.whichDay + 5);
+    } else if (hour == 14) {
+        document.getElementById("time").innerHTML = (85 - minutes) + " minutes left in period " + (window.whichDay + 5);
+    } else {
+        document.getElementById("time").innerHTML = "Classes over, click for clock";
     }
+
+    mainloop = setTimeout(gettime, 1000);
 }
 
+
 function startclock15() {
+    window.whichDay = 0;
+
     var elements = document.getElementsByClassName("disappear");
 
     for (var i of elements) {
         i.style.display = "none";
     };
 
-    gettime(0);
-    setInterval(gettime(0), 1000);
+    gettime()
 }
 
 function startclock610() {
+    window.whichDay = 5;
+
     var elements = document.getElementsByClassName("disappear");
 
     for (var i of elements) {
         i.style.display = "none";
     };
 
-    gettime(5);
-
-    setInterval(gettime(5), 1000);
+    gettime()
 }
 
 function displaytime() {
     var d = new Date();
-    console.log('hi')
+    console.log('clockloop working')
     hour = d.getHours()
     minutes = d.getMinutes()
     seconds = d.getSeconds()
@@ -68,6 +144,7 @@ function hide() {
     var clock = document.getElementById("clock");
     clock.style.display = ""
     displaytime();
+    clearTimeout(mainloop);
 }
 
 function show() {
@@ -75,5 +152,7 @@ function show() {
     time.style.display = ""
     var clock = document.getElementById("clock");
     clock.style.display = "none"
+
+    gettime()
     clearTimeout(clockloop);
 }
